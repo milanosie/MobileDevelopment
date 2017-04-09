d243f60205c598e4a2e71ef05e0a2cbe5d2f8788
Milan Steenwinkel <milan.steenwinkel@navara.nl>
2017-04-09 10:25:25 +0200

    XML for Add Homework

---
+++ app/src/main/java/milan/hva/com/homework/AddHomeworkActivityFragment.java
@@ -0,0 +1,22 @@
+package milan.hva.com.homework;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AddHomeworkActivityFragment extends Fragment {
+
+    public AddHomeworkActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_homework, container, false);
+    }
+}
